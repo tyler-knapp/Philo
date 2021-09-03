5e3f396ca30dd52b3c26f160f571b55b7325bd50
tyler <tylerknapp25@gmail.com>
2021-09-03 13:08:15 -0400

    update to work

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    private ShoppingCart cart;
+
+    public static void main(String[] args) {
+
+        
+
+
+    }
+}
---
+++ src/com/company/ShoppingCart.java
@@ -0,0 +1,37 @@
+package com.company;
+
+import java.math.BigDecimal;
+
+public class ShoppingCart {
+
+    private int itemQuantity;
+    private String itemName;
+    private BigDecimal itemPrice;
+
+
+
+
+    public int getItemQuantity() {
+        return itemQuantity;
+    }
+
+    public void setItemQuantity(int itemQuantity) {
+        this.itemQuantity = itemQuantity;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public BigDecimal getItemPrice() {
+        return itemPrice;
+    }
+
+    public void setItemPrice(BigDecimal itemPrice) {
+        this.itemPrice = itemPrice;
+    }
+}
